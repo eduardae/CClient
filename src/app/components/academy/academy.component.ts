2c270c1dbc5c9b0a01de3ce40907ae63d1ab0f14
eduardae <andar.edd@gmail.com>
2020-01-30 23:29:21 +0100

    academy section restyle

--- src/app/components/academy/academy.component.ts
+++ src/app/components/academy/academy.component.ts
@@ -13,7 +13,38 @@ export class AcademyComponent implements OnInit {
   isUpdating: boolean;
   response: string;
 
+  showTechnicalLinks: boolean = true;
+  showFinancialLinks: boolean = true;
+  showHowToGuides: boolean = false;
+  showProjectsLinks: boolean = true;
+  showBusinessSocialDev: boolean = true;
+  showCourses: boolean = false;
+
   constructor(private http: Http) {}
 
   ngOnInit() {}
+
+  triggerTechnicalTabCollapse() {
+    this.showTechnicalLinks = !this.showTechnicalLinks;
+  }
+
+  triggerFinancialTabCollapse() {
+    this.showFinancialLinks = !this.showFinancialLinks;
+  }
+
+  triggerHowToCollapse() {
+    this.showHowToGuides = !this.showHowToGuides;
+  }
+
+  triggerProjectsCollapse() {
+    this.showProjectsLinks = !this.showProjectsLinks;
+  }
+
+  triggerBusinesSocialDevCollapse() {
+    this.showBusinessSocialDev = !this.showBusinessSocialDev;
+  }
+
+  triggerCoursesCollapse() {
+    this.showCourses = !this.showCourses;
+  }
 }
